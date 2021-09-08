2203aa380ad916ec1caf6024308353ed00aaab45
Yang <delautre.damien.std@gmail.com>
2021-09-08 10:41:26 +0200

    negatif on shaderSC

--- shaderSC.fs
+++ shaderSC.fs
@@ -10,17 +10,17 @@ void main(void) {
 
 	vec4 col;
 	col[0]=texture2D(uSampler, vec2(tCoords.s, tCoords.t)).r;
-	col[1]=texture2D(uSampler, vec2(tCoords.s, tCoords.t)).g;
+	/* col[1]=texture2D(uSampler, vec2(tCoords.s, tCoords.t)).g;
 	col[2]=texture2D(uSampler, vec2(tCoords.s, tCoords.t)).b;
 	col[0]=col[0]+col[1];
 	col[0]=col[0]+col[2];
-	col[0]=(col[0]/3.0);
-	//col[0]=col[0]*(-1.0);
-	//col[0]=col[0]+1.0;
+	col[0]=(col[0]/3.0); */
+	col[0]=col[0]*(-1.0);
+	col[0]=col[0]+1.0;
 	col[1]=col[0];
 	col[2]=col[1];
-	if(col[0]>0.5) {
-		col[3]=alpha;
+	if(col[0]<0.5) {
+		col[3]=texture2D(uSampler, vec2(tCoords.s, tCoords.t)).r*alpha;
 	}else{
 		col[3]=0.0;
 	}
